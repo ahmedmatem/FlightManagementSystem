d12f1cd138876c560c771090811e5182a06cd7eb
TypxaH <turhan@abv.bg>
2024-07-04 11:33:49 +0300

    Properties and constructor created

--- Flight.cs
+++ Flight.cs
@@ -8,5 +8,30 @@ namespace FlightManagementSystem
 {
     public class Flight
     {
+        /*
+         •	flightID: уникален идентификатор на полета (низ или цяло число)
+•	destination: дестинация на полета (низ)
+•	departureTime: време на излитане (дата и/или време, може и низ)
+•	arrivalTime: време на пристигане (дата и/или време, може и низ)
+•	seatsAvailable: налични места (цяло число)
+•	price: цена на билет за полета (дробно число)
+
+         */
+        public string FlightID { get; private set; }
+        public string Destination { get; private set; }
+        public DateTime DepartureTime { get; private set; }
+        public DateTime ArrivalTime { get; private set; }
+        public int SeatsAvailable { get; private set; }
+        public decimal Price { get; private set; }
+
+        public Flight(string flightID, string destination, DateTime departureTime, DateTime arrivalTime, int seatsAvailable, decimal price)
+        {
+            FlightID = flightID;
+            Destination = destination;
+            DepartureTime = departureTime;
+            ArrivalTime = arrivalTime;
+            this.SeatsAvailable = seatsAvailable;
+            Price = price;
+        }
     }
 }
